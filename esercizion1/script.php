5c2cd6d8d0c982b973009702d82fad453f5aa4d1
NicoloMartiradonna <116425882+NicoloMartiradonna@users.noreply.github.com>
2023-10-01 10:38:56 +0200

    Add files via upload

---
+++ esercizion1/script.php
@@ -0,0 +1,15 @@
+<?php 
+
+$nome= 'Nicolò'
+$cognome='Martiradonna'
+echo "Il mio primo SCRIPT PHP";
+<br>
+ "Il mio nome è  $nome"
+<br>
+"Il mio cognome è $cognome"
+<br>
+ 'Vivo a Modugno'
+<br>
+'Provincia di Bari'
+
+?>
